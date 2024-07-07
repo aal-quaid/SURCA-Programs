b211e40acfd7d03963286cb771b206c9fbbdcfc5
aal-quaid <126218199+aal-quaid@users.noreply.github.com>
2024-07-07 14:33:49 -0400

    Update affineCryptanalysis_1.py

--- affineCryptanalysis_1.py
+++ affineCryptanalysis_1.py
@@ -1,28 +1,37 @@
 #This program finds the keys to the cipher with only the plainText and cipherText
 
-#Assume the user will enter 2 or more letters for each text
 import math
 
 def keys(P, C):
+    validValues = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]   #List of all possible values for A
+
     P = P.lower()
     C = C.lower()
 
-    P1 = ord(P[0]) - ord('a')
-    P2 = ord(P[1]) - ord('a')
-    C1 = ord(C[0]) - ord('a')
-    C2 = ord(C[1]) - ord('a')
+    for i in range(len(P)-1):
+        for j in range(i+1, len(P)):
 
-    D = (P1 - P2) % 26
+            P1 = ord(P[i]) - ord('a')
+            P2 = ord(P[j]) - ord('a')
+            C1 = ord(C[i]) - ord('a')
+            C2 = ord(C[j]) - ord('a')
 
-    if math.gcd(D, 26) != 1: #Checks to see if D is valid
-        return False, False
+            D = (P1 - P2) % 26
+            if math.gcd(D, 26) != 1:    #Checks to see if D is relatively prime to 26
+                continue
+
+            inverseD = inverse(D)
+
+            AKey = (inverseD * (C1 - C2)) % 26
+            BKey = (inverseD * (P1*C2 - P2*C1)) % 26
+
+            return AKey, BKey
     
-    inverseD = inverse(D)
 
-    A = (inverseD * (C1 - C2)) % 26
-    B = (inverseD * (P1*C2 - P2*C1)) % 26
-
-    return A, B
+    for A in validValues:               #Cycle through all possible values for A
+        if C1 - C2 == (A * (P1-P2)) % 26:
+            B = (C1 - (P1*A)) % 26      #Substite A into one of the equations to find B
+            return A, B
 
 def inverse(a):
     for num in range(1,26):
@@ -36,9 +45,6 @@ def main():
     
     A, B = keys(P, C)
 
-    if A == False:
-        print("Invalid Input. Please try again.")
-    else:
-        print(f"\nThe Keys are: A = {A} and B = {B}")
+    print(f"\nThe Keys are: A = {A} and B = {B}")
 
 main()
